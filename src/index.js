b66965bf1182919bf047b1c4a4715404112a9a78
Alex Rujan <alexrujan3@gmail.com>
2025-09-22 12:48:03 +0300

    Changes at arrow function to scrool faster

--- src/index.js
+++ src/index.js
@@ -123,7 +123,7 @@ function setupEventListeners() {
 
     const cards_wrapper = document.querySelector(".cards-list-wrapper");
     const width_card_wrapper =
-      Math.floor(cards_wrapper.getBoundingClientRect().width) - width_card;
+      Math.floor(cards_wrapper.getBoundingClientRect().width) - 50;
 
     const translateX = parseFloat(
       getComputedStyle(cards_list).transform.split(", ")[4]
